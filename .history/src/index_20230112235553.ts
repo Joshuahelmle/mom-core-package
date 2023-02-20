fda87c8fdbe5236940459a24c50b117f4ee4b230
Joshuahelmle <joshua.helmle@htwg-konstanz.de>
2023-02-20 17:24:03 +0100

    added gitignore, fixed repo setup

--- .history/src/index_20230112235553.ts
+++
@@ -1,6 +0,0 @@
-import { ITEMS, Item } from "./entities/Item";
-import { BUILDINGS, Building } from "./entities/Building";
-import { RECIPES, Recipe, findRecipesWithInput, findRecipesWithOutput } from "./entities/Recipe";
-import { LOOTTABLES, LootTable, fetchedPoolData } from "./entities/LootTable";
-
-export { ITEMS, Item, LOOTTABLES, LootTable, fetchedPoolData, BUILDINGS, Building, RECIPES, Recipe, findRecipesWithInput, findRecipesWithOutput }
